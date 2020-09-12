5987f2e8a8fc33549bfe86a416823a5ba564e2d9
Eddges <shivamshekhar.i7@gmail.com>
2020-09-12 20:17:08 +0530

    Modified Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import LogoBlack from '../../assets/LogoBlack.png'
 import {NavLink} from 'react-router-dom'
 // =======
 // import LogoBlack from '../../assets/LogoBlack.png';
-// import Portal from '../../components/Portal/Portal';
+import Portal from '../../components/Portal/Portal';
 // >>>>>>> bbefccee4b9b8e4eff56b2465134c1dc5ab1cefb
 
 const Navbar = (props) => {
