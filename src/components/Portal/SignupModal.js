a5ea84f13c0db94096bd6e533cd1b7fefab14064
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-12 22:23:49 +0530

    signup modal

---
+++ src/components/Portal/SignupModal.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import styles from './modal.module.css';
+import SignupContent from './SignupContent';
+
+
+
+const Button_el = {
+    border:'none',
+    backgroundColor:'gray',
+    fontWeight:600,
+    color:'white',
+    marginLeft:'1em',
+    left:'1em',
+
+}
+
+
+
+function ModalSignUp({ open , children, onClose}) {
+    if(!open) return null;
+
+    return ReactDom.createPortal (
+        <>
+          <div className = {styles.Overlay_Styles}/>
+          <div className = {styles.Modal_Styles}>
+            
+            <SignupContent onClose = {onClose} />
+            </div>
+            
+        </>,
+        document.getElementById('portal')
+      
+    )
+}
+
+export default ModalSignUp;
