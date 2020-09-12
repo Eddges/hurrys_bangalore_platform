bbefccee4b9b8e4eff56b2465134c1dc5ab1cefb
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-12 19:09:14 +0530

    minor changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Navbar.module.css'
-import LogoBlack from '../../assets/LogoBlack.png'
+import LogoBlack from '../../assets/LogoBlack.png';
+import Portal from '../../components/Portal/Portal';
 
 const Navbar = (props) => {
     return(
         <div className={classes.Navbar}>
             <img className={classes.Logo} src={LogoBlack} alt="Logo" />
             <div className={classes.NavButtons}>
-                <button className={classes.LoginButton}>Login</button>
+                <button className={classes.LoginButton}> <Portal/> </button>
                 <button className={classes.SignUpButton}>Sign Up</button>
             </div>
         </div>
