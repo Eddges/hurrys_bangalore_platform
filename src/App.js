46fe96dbcb2a25eee4c1d7276c9fd6795237a618
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-18 23:37:55 +0530

    testing push

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Delivery from './components/Delivery/Delivery';
 import Homepage from './components/Homepage/Homepage';
 import ProductsGrocery from './components/ProductsGrocery/ProductsGrocery';
 import ShopsAvailable from './components/ShopsAvailable/ShopsAvailable';
+import OrderConfirm from './components/orderConfirm/OrderConfirm';
+import ReferralPage from './components/Referral/ReferralPage';
 
 class App extends React.Component{
   render(){
@@ -18,6 +20,8 @@ class App extends React.Component{
         <Route path="/vendor" exact render={() => <Vendor />} />
         <Route path="/grocery" exact render={() => <ProductsGrocery />} />
         <Route path="/shops" exact render={() => <ShopsAvailable />} />
+        <Route path="/orderconfirmed" exact render={() =>  <OrderConfirm/> } />
+        <Route path="/referral" exact render={() =>  <ReferralPage/> } />
       </div>
     )
   }
